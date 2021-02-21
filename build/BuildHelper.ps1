27a4589b7276ac280407d7b11a8f4a5be7a7f886
Sanjay <sanjay.mnambiar@gmail.com>
2021-02-21 01:02:15 +0100

    Fixed errors in generate artifacts script

--- build/BuildHelper.ps1
+++ build/BuildHelper.ps1
@@ -25,12 +25,14 @@ function Generate-Artifact
 	if (Test-Path -Path $WorkDir -PathType Container) { Remove-Item -LiteralPath $WorkDir -Force -Recurse }
 	New-Item -ItemType Directory -Force -Path $WorkDir
 
+	Remove-Item -LiteralPath "$ArtifactDir/$PluginName.zip" -Force
+
 	New-Item -ItemType Directory -Force -Path "$WorkDir/plugins/settings"
 	Copy-Item "$AssemblyDir/$PluginName.dll" -Destination "$WorkDir/plugins"
 	Copy-Item -Path "$ConfigDir/$PluginName.set" -Destination "$WorkDir/plugins/settings"
 
-	New-Item -ItemType Directory -Force -Path "$WorkDir/data"
-	Copy-Item -Path "$ConfigDir/placeholder.jpg" -Destination "$WorkDir/data"
+	New-Item -ItemType Directory -Force -Path "$WorkDir/data/$PluginName"
+	Copy-Item -Path "$ConfigDir/placeholder.jpg" -Destination "$WorkDir/data/$PluginName"
 
 	Compress-Archive -Path "$WorkDir/*" -DestinationPath "$ArtifactDir/$PluginName.zip"
 
